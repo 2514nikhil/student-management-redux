c53e58d102b655ae17fde93c8231b81d4e651ba7
2514nikhil <nikhilmaghodiya25@gmail.com>
2025-03-25 09:43:04 +0530

    store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import studentReducer from '../Features/studentsSlice';
+import studentReducer from './Features/studentsSlice';
 
 
 const store = configureStore({
